e4067bdb88ac38c6997a63b1002797b7f1a4dfe5
M Riezky Nurfajri <riezkynurfajri@gmail.com>
2022-06-17 09:36:42 +0700

    Revert "added: mkdir(server/services server/orchestrator) init orchestrator, init tailwind on client side"
    This reverts commit 9b74ae47fc9918c295e4c8f5e8a73b5a380da258.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import logo from "./logo.svg"
-import "./App.css"
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className={"text-rose-400"}>
-          Edit <code className={"text-emerald-400"}>src/App.js</code> and save to reload.
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className={"text-sky-500"}
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
