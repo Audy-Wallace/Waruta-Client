bb19f5de72e010cd0b64173c54f6c9eee1df4eb2
M Riezky Nurfajri <riezkynurfajri@gmail.com>
2022-06-18 11:00:33 +0700

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom"
 import "./App.css"
 import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <div id="root" className="bg-repeat min-h-screen bg-gradient-to-br from-[#c236e2] to-[#5719dc]">
+    <div id="root" className="bg-repeat min-h-screen bg-gradient-to-tl from-[#B689C0] to-[#6A67CE]">
       <NavBar />
+      <Routes></Routes>
     </div>
   )
 }
