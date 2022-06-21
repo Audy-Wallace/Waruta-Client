c632f77b53c82866d0219c5ee87373efd3daf234
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-06-21 19:51:12 +0700

    feat: fetch leaderboard

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,5 +1,6 @@
-import {legacy_createStore as createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer";
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 export default store;
