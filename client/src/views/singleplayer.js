52f721815aaa87b609c8e9a32725bb96a1c960e6
Audy-Wallace <94215626+Audy-Wallace@users.noreply.github.com>
2022-06-22 11:47:48 +0700

    fix bug timer

--- client/src/views/singleplayer.js
+++ client/src/views/singleplayer.js
@@ -4,208 +4,282 @@ import { fetchWords } from "../stores/actions/wordAction"
 import { useDispatch, useSelector } from "react-redux"
 import Timer from "../components/Timer"
 import Voice from "../components/Voice"
+import useSound from "use-sound";
+import boopSfx from "../sounds/Applause.mp3";
+import JSConfetti from "js-confetti";
+import { makeLeaderboard } from "../stores/actions/leaderboardAction";
+import { useNavigate } from "react-router-dom";
 const Singleplayer = () => {
-  const dispatch = useDispatch()
-  const { words, solution } = useSelector((state) => state.words)
-  const [answer, setAnswer] = React.useState("")
-  const [guesses, setGuesses] = React.useState(6)
-  const [pastAnswers, setPastAnswers] = React.useState([])
-  const [isCorrect, setIsCorrect] = React.useState(false)
-  const [localWords, setLocalWords] = React.useState([])
-  const [localSolution, setLocalSolution] = React.useState("")
-  const [remainSeconds, setRemainSeconds] = React.useState(0)
-  const [userScore, setUserScore] = React.useState(0)
-  const [open, setOpen] = React.useState(false)
-  const [hint, setHint] = React.useState(false)
-  const [wrong, setWrong] = React.useState(false)
-  const [timeup, setTimeup] = React.useState(false)
-  const [lose, setLose] = React.useState(false)
-  const [answerByVoice, setAnswerByVoice] = React.useState(false)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { words, solution } = useSelector((state) => state.words);
+  const [answer, setAnswer] = React.useState("");
+  const [guesses, setGuesses] = React.useState(6);
+  const [pastAnswers, setPastAnswers] = React.useState([]);
+  const [isCorrect, setIsCorrect] = React.useState(false);
+  const [localWords, setLocalWords] = React.useState([]);
+  const [localSolution, setLocalSolution] = React.useState("");
+  const [remainSeconds, setRemainSeconds] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
+  const [hint, setHint] = React.useState(false);
+  const [wrong, setWrong] = React.useState(false);
+  const [timeup, setTimeup] = React.useState(false);
+  const [lose, setLose] = React.useState(false);
+  const [answerByVoice, setAnswerByVoice] = React.useState(false);
+  const [gameDone, setGameDone] = React.useState(false);
+  const [play] = useSound(boopSfx);
+  const jsConfetti = new JSConfetti();
   function answerHandler(e) {
-    setAnswer(e.target.value)
+    setAnswer(e.target.value);
   }
-
   function answerVoice(finalTranscript) {
-    finalTranscript = finalTranscript.replace(".", "")
+    finalTranscript = finalTranscript.replace(".", "");
     if (finalTranscript == "11") {
-      finalTranscript = "Seblak"
+      finalTranscript = "Seblak";
     }
-    setAnswer(finalTranscript)
-    setAnswerByVoice(true)
+    setAnswer(finalTranscript);
+    setAnswerByVoice(true);
   }
-
   React.useEffect(() => {
     if (answerByVoice) {
-      autoEnter()
-      setAnswerByVoice(false)
+      autoEnter();
+      setAnswerByVoice(false);
     }
-  }, [answerByVoice])
+  }, [answerByVoice]);
 
   function autoEnter() {
     if (answer && guesses > 0) {
-      const remainingGuesses = guesses - 1
-      console.log(remainingGuesses, "REMAINING GUESSES")
-      setGuesses(remainingGuesses)
+      const remainingGuesses = guesses - 1;
+      console.log(remainingGuesses, "REMAINING GUESSES");
+      setGuesses(remainingGuesses);
       // when the user has guessed the user's remaining guesses is stored in localStorage
       // if user reloads the page, he/she will still have the same number of remaining guesses
-      localStorage.setItem("user_guesses", remainingGuesses)
-      console.log(answer, "ANSWER")
-      const userGuess = localWords.find((el) => el.name.toLowerCase() === answer.toLowerCase())
+      localStorage.setItem("user_guesses", remainingGuesses);
+      console.log(answer, "ANSWER");
+      const userGuess = localWords.find(
+        (el) => el.name.toLowerCase() === answer.toLowerCase()
+      );
       if (userGuess) {
-        const keys = Object.keys(userGuess)
-        const obj = {}
-        let allCorrect = true
+        const keys = Object.keys(userGuess);
+        const obj = {};
+        let allCorrect = true;
         for (let i = 0; i < keys.length; i++) {
-          const key = keys[i]
-          if (key === "id") continue
-          if (userGuess.name.toLowerCase() !== localSolution.name.toLowerCase()) {
+          const key = keys[i];
+          if (key === "id") continue;
+          if (
+            userGuess.name.toLowerCase() !== localSolution.name.toLowerCase()
+          ) {
             obj[key] = {
               value: userGuess[key],
               isCorrect: false,
-            }
-            allCorrect = false
-            continue
+            };
+            allCorrect = false;
+            continue;
           }
           if (userGuess[key] !== localSolution[key]) {
             obj[key] = {
               value: userGuess[key],
               isCorrect: false,
-            }
-            allCorrect = false
-            continue
+            };
+            allCorrect = false;
+            continue;
           }
           obj[key] = {
             value: userGuess[key],
             isCorrect: true,
-          }
+          };
         }
-        const temp = [...pastAnswers, obj]
-        setPastAnswers(temp)
-        localStorage.setItem("pastAnswers", JSON.stringify(temp))
-        if (allCorrect) setIsCorrect(true)
+        const temp = [...pastAnswers, obj];
+        setPastAnswers(temp);
+        localStorage.setItem("pastAnswers", JSON.stringify(temp));
+        if (allCorrect) setIsCorrect(true);
       } else {
         // if the user's answer does not exist do something
-        console.log("food does not exist")
+        console.log("food does not exist");
       }
-      setAnswer("")
+      setAnswer("");
     }
   }
 
   function onEnter(e) {
     // user can only submit if answer is truthy and guesses are above 0
     if (e.key === "Enter" && answer && guesses > 0) {
-      const remainingGuesses = guesses - 1
-      if (remainingGuesses === 0) setLose(true)
-      setGuesses(remainingGuesses)
+      const remainingGuesses = guesses - 1;
+      if (remainingGuesses === 0) setLose(true);
+      setGuesses(remainingGuesses);
       // when the user has guessed the user's remaining guesses is stored in localStorage
       // if user reloads the page, he/she will still have the same number of remaining guesses
-      localStorage.setItem("user_guesses", remainingGuesses)
-      const userGuess = localWords.find((el) => el.name.toLowerCase() === answer.toLowerCase())
+      localStorage.setItem("user_guesses", remainingGuesses);
+      const userGuess = localWords.find(
+        (el) => el.name.toLowerCase() === answer.toLowerCase()
+      );
       if (userGuess) {
-        const keys = Object.keys(userGuess)
-        const obj = {}
-        let allCorrect = true
+        const keys = Object.keys(userGuess);
+        const obj = {};
+        let allCorrect = true;
         for (let i = 0; i < keys.length; i++) {
-          const key = keys[i]
-          if (key === "id") continue
-          if (userGuess.name.toLowerCase() !== localSolution.name.toLowerCase()) {
+          const key = keys[i];
+          if (key === "id") continue;
+          if (
+            userGuess.name.toLowerCase() !== localSolution.name.toLowerCase()
+          ) {
             obj[key] = {
               value: userGuess[key],
               isCorrect: false,
-            }
-            allCorrect = false
-            continue
+            };
+            allCorrect = false;
+            continue;
           }
           if (userGuess[key] !== localSolution[key]) {
             obj[key] = {
               value: userGuess[key],
               isCorrect: false,
-            }
-            allCorrect = false
-            continue
+            };
+            allCorrect = false;
+            continue;
           }
           obj[key] = {
             value: userGuess[key],
             isCorrect: true,
-          }
+          };
         }
-        const temp = [...pastAnswers, obj]
-        setPastAnswers(temp)
-        localStorage.setItem("pastAnswers", JSON.stringify(temp))
-        if (allCorrect) setIsCorrect(true)
+        const temp = [...pastAnswers, obj];
+        setPastAnswers(temp);
+        localStorage.setItem("pastAnswers", JSON.stringify(temp));
+        if (allCorrect) setIsCorrect(true);
       } else {
         // if the user's answer does not exist do something
-        setWrong(true)
+        setWrong(true);
       }
-      setAnswer("")
+      setAnswer("");
     }
   }
   React.useEffect(() => {
-    dispatch(fetchWords())
-  }, [])
+    dispatch(fetchWords());
+  }, []);
   React.useEffect(() => {
     // If user is correct do something
     if (isCorrect) {
-      localStorage.setItem("win", true)
-      localStorage.setItem("remainingTime", remainSeconds)
-      const totalGuesses = +localStorage.getItem("user_guesses")
-      let guessScore
+      play();
+      localStorage.setItem("win", true);
+      localStorage.setItem("remainingTime", remainSeconds);
+      const totalGuesses = +localStorage.getItem("user_guesses");
+      let guessScore;
       switch (totalGuesses) {
         case 5:
-          guessScore = 60
-          break
+          guessScore = 60;
+          break;
         case 4:
-          guessScore = 50
-          break
+          guessScore = 50;
+          break;
         case 3:
-          guessScore = 40
-          break
+          guessScore = 40;
+          break;
         case 2:
-          guessScore = 30
-          break
+          guessScore = 30;
+          break;
         case 1:
-          guessScore = 20
-          break
+          guessScore = 20;
+          break;
         default:
-          guessScore = 10
-          break
+          guessScore = 10;
+          break;
       }
-      let timeScore
-      const secondsLeft = +localStorage.getItem("remainingTime")
+      let timeScore;
+      const secondsLeft = +localStorage.getItem("remainingTime");
 
       if (secondsLeft >= 240 && secondsLeft <= 300) {
-        timeScore = 60
+        timeScore = 60;
       } else if (secondsLeft >= 180) {
-        timeScore = 50
+        timeScore = 50;
       } else if (secondsLeft >= 120) {
-        timeScore = 40
+        timeScore = 40;
       } else if (secondsLeft >= 60) {
-        timeScore = 30
+        timeScore = 30;
       } else if (secondsLeft >= 30) {
-        timeScore = 20
+        timeScore = 20;
       } else if (secondsLeft >= 0) {
-        timeScore = 10
+        timeScore = 10;
       }
-      const score = guessScore + timeScore
-      localStorage.setItem("score", score)
-      setOpen(true)
-    }
-    if (isCorrect === false && +localStorage.getItem("user_guesses") === 0) {
-      localStorage.setItem("score", 0)
-    }
-    if (isCorrect === false && +localStorage.getItem("remainingTime") === 0) {
-      localStorage.setItem("score", 0)
-    }
-  }, [isCorrect])
-  React.useEffect(() => {
-    setLocalWords(words)
-  }, [words])
-  React.useEffect(() => {
-    setLocalSolution(solution)
-  }, [solution])
 
+      const score = guessScore + timeScore;
+      localStorage.setItem("score", score);
+      setOpen(true);
+      setGameDone(true);
+      jsConfetti.addConfetti({
+        confettiRadius: 2,
+        confettiNumber: 100,
+        emojis: ["🍔", "🥓", "🍟", "🍣"],
+        emojiSize: 60,
+      });
+    }
+    if (
+      isCorrect === false &&
+      +localStorage.getItem("user_guesses") === 0 &&
+      localStorage.getItem("user_guesses") !== null
+    ) {
+      localStorage.setItem("win", false);
+      localStorage.setItem("score", 0);
+      setGameDone(true);
+    }
+    if (
+      isCorrect === false &&
+      +localStorage.getItem("remainingTime") === 0 &&
+      !localStorage.getItem("win")
+    ) {
+      localStorage.setItem("win", false);
+      localStorage.setItem("score", 0);
+      setGameDone(true);
+    }
+  }, [
+    isCorrect,
+    localStorage.getItem("user_guesses"),
+    localStorage.getItem("remainingTime"),
+  ]);
+  React.useEffect(() => {
+    setLocalWords(words);
+  }, [words]);
+  React.useEffect(() => {
+    setLocalSolution(solution);
+  }, [solution]);
+  React.useEffect(() => {
+    if (localStorage.getItem("access_token") && gameDone) {
+      dispatch(
+        makeLeaderboard({
+          score: localStorage.getItem("score"),
+          guess: +localStorage.getItem("user_guesses"),
+          time: 300 - +localStorage.getItem("remainingTime"),
+        })
+      );
+    }
+  }, [gameDone]);
   function close() {
-    setOpen(false)
+    setOpen(false);
+  }
+  function removeItem() {
+    localStorage.removeItem("index");
+    localStorage.removeItem("score");
+    localStorage.removeItem("user_guesses");
+    localStorage.removeItem("remainingTime");
+    localStorage.removeItem("win");
+    localStorage.removeItem("pastAnswers");
+    localStorage.removeItem("time");
+  }
+  function goBackHome() {
+    return (
+      <button
+        type="button"
+        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+        onClick={() => {
+          setTimeup(false);
+          setLose(false);
+          removeItem();
+          navigate("/", { replace: true });
+        }}
+      >
+        Go Back Home
+      </button>
+    );
   }
   return (
     <div className="flex flex-col items-center">
@@ -388,13 +462,7 @@ const Singleplayer = () => {
                 </div>
 
                 <div className="mt-4 w-full flex justify-center">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={close}
-                  >
-                    Got it, thanks!
-                  </button>
+                {goBackHome()}
                 </div>
               </Dialog.Panel>
             </div>
@@ -434,13 +502,7 @@ const Singleplayer = () => {
                 {/* //? num of guesses */}
 
                 <div className="mt-4 w-full flex justify-center">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setTimeup(false)}
-                  >
-                    close
-                  </button>
+                {goBackHome()}
                 </div>
               </Dialog.Panel>
             </div>
@@ -481,13 +543,7 @@ const Singleplayer = () => {
                 {/* //? num of guesses */}
 
                 <div className="mt-4 w-full flex justify-center">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setLose(false)}
-                  >
-                    close
-                  </button>
+                {goBackHome()}
                 </div>
               </Dialog.Panel>
             </div>
