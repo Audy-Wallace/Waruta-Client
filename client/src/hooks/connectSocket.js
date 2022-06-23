f9e84fc85bfe869f87c9b4d695fe23c8d99857aa
redyRenno <redyrenno@gmail.com>
2022-06-23 22:42:02 +0900

    deploy app v2

--- client/src/hooks/connectSocket.js
+++ client/src/hooks/connectSocket.js
@@ -1,11 +1,10 @@
 import { createContext, useState, useRef, useEffect } from "react"
 import { io } from "socket.io-client"
-import Peer from "simple-peer"
 
 const SocketContext = createContext()
 
 function connectSocket() {
-  const socket = io.connect("http://localhost:3000/") // https://api-waruta.herokuapp.com/
+  const socket = io.connect("https://api-waruta.herokuapp.com/")
   return socket
 }
 
